4921e2e9a84b99f4596d69983cdbe2d7c43612ab
LabCap <banlazthosow@gmail.com>
2022-08-29 15:08:39 +0300

    v4

--- script.js
+++ script.js
@@ -1,21 +1,48 @@
-const colors = [
-  "rgb(255, 151, 151)",
-  "rgb(151, 255, 165)",
-  "rgb(151, 196, 255)",
-];
-
 const wrapper = document.querySelector(".wrapper"),
   text = document.querySelector(".text"),
-  randomColorBtn = document.querySelector(".random-color-btn");
+  textLabel = document.querySelector(".text-label"),
+  colorBtn = document.querySelectorAll(".color-btn"),
+  colorRandomBtn = document.querySelector(".color-btn-random"),
+  colorAddBtn = document.querySelector(".color-btn-add");
+// inputColor = document.querySelector(".input-color");
 
-let randomColor = Math.floor(Math.random() * 3);
+let colors = ["rgb(255, 151, 151)", "rgb(151, 255, 165)", "rgb(151, 196, 255)"];
 
-console.log(randomColor);
+colorRandomBtn.addEventListener("click", () => {
+  let randomColor = Math.floor(Math.random() * colors.length);
 
-wrapper.style.cssText = `background: ${colors[randomColor]};`;
-text.style.cssText = `color: ${colors[randomColor]};`;
-randomColorBtn.style.cssText = `
-  background: ${colors[randomColor]};
-  color: ${colors[randomColor]};
-`;
-text.innerText = `${colors[randomColor]}`;
+  for (let i = 0; i < colorBtn.length; i++) {
+    const thisColorBtn = colorBtn[i];
+
+    wrapper.style.cssText = `background: ${colors[randomColor]};`;
+    textLabel.style.cssText = `color: ${colors[randomColor]};`;
+    text.style.cssText = `color: ${colors[randomColor]};`;
+    thisColorBtn.style.cssText = `
+      background: ${colors[randomColor]};
+      color: ${colors[randomColor]};
+    `;
+  }
+
+  text.innerText = `${colors[randomColor]}`;
+});
+
+colorAddBtn.addEventListener("click", () => {
+  let inputColorValue = document.querySelector(".input-color").value;
+
+  if (inputColorValue) {
+    colors.push(inputColorValue);
+    for (let i = 0; i < colorBtn.length; i++) {
+      const thisColorBtn = colorBtn[i];
+
+      wrapper.style.cssText = `background: ${inputColorValue};`;
+      textLabel.style.cssText = `color: ${inputColorValue};`;
+      text.style.cssText = `color: ${inputColorValue};`;
+      thisColorBtn.style.cssText = `
+    background: ${inputColorValue};
+      color: ${inputColorValue};
+      `;
+    }
+    inputColor.value = "";
+  } else {
+  }
+});
